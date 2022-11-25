45ae6f4325c3afca78ba6123fef628b128c9ad73
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-11-25 14:50:08 +0200

    06
    06 uzduotis su $argv

--- 06.php
+++ 06.php
@@ -19,3 +19,19 @@ if (is_numeric($numOne) === false || !(int)$numTwo) {
 
 $result = ($numOne * $numTwo)**2;
 echo 'Jusu skaicius: ' . $result;
+
+
+//========================================================================================
+// su $argv :
+
+
+// $number = (int)$argv[1];
+// $number2 = (int)$argv[2];
+// if (is_numeric($argv[1]) === false && is_numeric($argv[2]) === false) {
+//     echo "ivestas ne skaicius" . PHP_EOL;
+//     exit(1);
+// }
+
+// $kvadratas = ($number * $number2)**2;
+// echo 'jusu skaicius: ' . $kvadratas;
+
