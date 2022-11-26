4a4450c8c5c1d64db7ed53c6056633caec0d0f4d
smoking-kills-airbases <gelzbetonis369@gmail.com>
2022-11-26 19:51:07 +0200

    07
    septinta uzduotis daug or tikriausiai reiketu kordinates i array susideti

--- 07/index.php
+++ 07/index.php
@@ -4,35 +4,49 @@
 	<style>
     	table {
         	border: 2px solid black;
-            border-collapse: collapse;
+            /* border-collapse: collapse; */
         }
         td {
         	height: 50px;
             width: 50px;
+            border: 1px solid black;
         }
         .black {
-        	background-color: black;
+        	background-color: burlywood;
         }
     </style>
 </head>
 
 <body>
-    <table>
-        <?php
+<table>
+<?php
 
-            for ($rows = 1; $rows <= 8; $rows++) {
-                echo "<tr>";
-                    for ($columns = 1; $columns  <= 8; $columns++) {
-                        if (($rows + $columns) %2 ==0) {
-                            echo "<td class='black'</td>";
-                        } else {
-                            echo "<td></td>";
-                        }
-                    }
-                echo "</tr>";
+	 for ($rows = 1; $rows <= 7;$rows++) {
+     	echo "<tr>";
+        	for ($columns = 1; $columns  <= 7;$columns++) {
+            	if ($rows == 4 && $columns == 4
+                	|| $rows == 1 && $columns == 1
+                    || $rows == 2 && $columns == 2
+                    || $rows == 3 && $columns == 3
+                    || $rows == 3 && $columns == 5
+                    || $rows == 2 && $columns == 6
+                    || $rows == 1 && $columns == 7
+                    || $rows == 7 && $columns == 1
+                    || $rows == 6 && $columns == 2
+                    || $rows == 5 && $columns == 3
+                    || $rows == 5 && $columns == 5
+                    || $rows == 6 && $columns == 6
+                    || $rows == 7 && $columns == 7
+                ) {
+                	echo "<td class='black'</td>";
+                } else {
+            		echo "<td></td>";
+                }  
             }
+        echo "</tr>";
+     }
 
-        ?>
-    </table>
+?>
+</table>
 </body>
 </html>
